9628e67bec025f7a191e045bd1f6814638b66f18
Dan Chiev <danchiev2@Gmail.com>
2020-11-04 20:48:26 -0500

    new commits

--- app.js
+++ app.js
@@ -14,15 +14,54 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+const teamObj = [{}]
+async function init () {
+ const answers = await inquirer.prompt(questions);
+    console.log(answers);
+    teamObj.push(answers);
+}
+const questions = [
+{
+    type: "input",
+    name: "name",
+    message: "What is the team member's name?"
+},
+{
+    type: "list",
+    name: "job",
+    choices: ["Manager","Engineer", "Intern"],
+},
+{
+    type: "input",
+    name: "id",
+    message: "What is this role's ID?",
+},
+{
+    type: "input",
+    name: "email",
+    message: "What is this role's email?",
+},
+{
+    type: "input",
+    name: "github",
+    message: "What is this role's GitHub username?",
+},
+
+]
+init();
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
